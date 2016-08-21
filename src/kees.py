9bdde1209484956467a5c44cf02bd558ab319b30
proprefenetre <nielseigenraam@gmail.com>
2016-08-21 22:05:57 +0200

    better error handling

--- src/kees.py
+++ src/kees.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from bs4 import BeautifulSoup
-from bs4 import FeatureNotFound
+from bs4 import BeautifulSoup, FeatureNotFound
 
 from re import sub
 
@@ -14,6 +13,7 @@ from urllib.error import HTTPError
 SEARCH_URL = 'http://www.mijnwoordenboek.nl/vertaal/{from}/{target}/{word}'
 LANGUAGES = ['NL', 'EN', 'DE', 'FR', 'ES']
 
+# style attrib of the font tag that contains the translation
 FONT_STYLE = 'color:navy;font-size:10pt'
 # div that contains the 'other sources' translations:
 TARGET_DIV = '.span8 > div:nth-of-type(1)'
@@ -44,43 +44,48 @@ def _get_other_sources(soup):
     tables = soup.select(TARGET_DIV + '> table')
     other_sources = []
     for t in tables:
+        # if 'border' in t:     # does not return anything
         if t.get('border', None) is not None:
             for td in t('td'):
+                # if 'style' in td:
                 if td.get('style', None) is not None:
                     other_sources.extend([w for w in
                                           td.text.split(';')])
     return other_sources
 
-# TODO: change this: drop the det, keep one
-# def _remove_dubs(lst):
-#     """ if translation occur twice, prefer the one with a determiner """
-#     words = [w.split() for w in lst]
-#     uniques = []
-#     keys = set()
-#     for word in words:
-#         if len(word) > 1:
-#             keys.add(word[-1])
-#             uniques.append(word)
-#     for word in [w for w in words if len(w) == 1]:
-#         if word[0] not in keys:
-#             uniques.append(word)
-#     return [' '.join(w) for w in uniques]
+
+def _remove_dubs(lst):
+    """ if translation occur twice, prefer the one with a determiner """
+    words = [w.split() for w in lst]
+    uniques = []
+    keys = set()
+    for word in words:
+        if len(word) > 1:
+            keys.add(word[-1])
+            uniques.append(word)
+    for word in [w for w in words if len(w) == 1]:
+        if word[0] not in keys:
+            uniques.append(word)
+    return [' '.join(w) for w in uniques]
 
 
 def _process(trs):
-    result = []
+    clean_words = []
     for t in trs:
         stripped = t.strip()
         subbed = sub(r'\s+[\(].*[\)]', '', stripped)
-        result.append(subbed)
-    # return _remove_dubs(result)
-    return result
+        clean_words.append(subbed)
+        no_dubs = _remove_dubs(clean_words)
+    return no_dubs
 
 
 def _parse_elements(args):
     soup = _get_soup(args)
-    translations = _process(_get_translations(soup) + _get_other_sources(soup))
-    return set(translations)
+    translations = _process(_get_translations(soup) +
+                            _get_other_sources(soup))
+    if args['sort']:
+        translations.sort()
+    return translations
 
 
 def translate(args):
@@ -88,24 +93,26 @@ def translate(args):
     args['from'] = args['from'].upper()
     args['target'] = args['target'].upper()
 
-    trs = list(_parse_elements(args))
+    translations = _parse_elements(args)
 
     if not any([args['target'] in LANGUAGES, args['from'] in LANGUAGES]):
         raise ValueError('{} - {} not available'.format(args['source'],
                                                         args['target']))
 
     if not any([args['target'] == 'NL', args['from'] == 'NL']):
-        raise ValueError('Either source or target language should be "NL"')
+        raise ValueError('Error: Either source or target language should be '
+                         '"NL"')
 
     if args['sort']:
-        trs.sort()
+        translations.sort()
 
     # TODO: move this to its own function
-    print('{}: ({} translations)'.format(args['word'], len(trs)))
-    num = args.get('num_translations', len(trs))
-    for idx, word in enumerate(trs):
+    print('{}: ({} translations)'.format(args['word'], len(translations)))
+    num = args.get('num_translations', len(translations))
+    for idx, word in enumerate(translations):
         pad = 2 if idx+1 < 10 else 3
         print('{:.<{}} {:<10}'.format(idx+1, pad, word))
         if idx+1 == num:
             break
+
     print()
